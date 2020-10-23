37eae7fb729c30c7c85b7d712294f5a06560ff91
ridhoalrafi1009 <rihdoalrafi@gmail.com>
2020-10-24 00:41:34 +0700

    Tugas

--- RidhoListView/app/src/main/java/com/example/ridholistview/HeroAdapter.java
+++ RidhoListView/app/src/main/java/com/example/ridholistview/HeroAdapter.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class HeroAdapter extends BaseAdapter {
     private Context context;
     private ArrayList<Hero> heroes = new ArrayList<>();
@@ -51,7 +53,7 @@ public class HeroAdapter extends BaseAdapter {
     private class ViewHolder {
         private TextView txtName;
         private TextView txtDescription;
-        private ImageView imgPhoto;
+        private CircleImageView imgPhoto;
         ViewHolder(View view) {
             txtName = view.findViewById(R.id.txt_name);
             txtDescription = view.findViewById(R.id.txt_description);
